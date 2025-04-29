03bd582432e7cffa28cf5f58bf596cf8c30ea6ff
Bikash <info.bikash9@gmail.com>
2025-04-29 17:25:47 +0530

    Manage the constants in project.

--- src/test/java/com/automationteststore/base/Constants.java
+++ src/test/java/com/automationteststore/base/Constants.java
@@ -1,6 +1,7 @@
 package com.automationteststore.base;
 
 public class Constants {
-	
+	private static final String PROJECT_FOLDER_PATH = System.getProperty("user.dir");
+	public static final String PROPERTIES_FILE_PATH = PROJECT_FOLDER_PATH+"\\src\\test\\resources\\config.properties";
 
 }
