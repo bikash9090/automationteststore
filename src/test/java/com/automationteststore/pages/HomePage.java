acba04ba9eb9023caad55da2af1014e46e4146ea
Bikash <info.bikash9@gmail.com>
2025-04-29 16:09:50 +0530

    Added test cases for HomePage

--- src/test/java/com/automationteststore/pages/HomePage.java
+++ src/test/java/com/automationteststore/pages/HomePage.java
@@ -1,5 +1,24 @@
 package com.automationteststore.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.automationteststore.base.PageBase;
+
+public class HomePage extends PageBase {
+
+	@FindBy(partialLinkText = "Login or register")
+	WebElement log_Register_btn;
+
+	
+	public HomePage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+	}
+
+	public void clickOn_Login_RegisterBtn() {
+		log_Register_btn.click();
+	}
 }
