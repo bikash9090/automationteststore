ef9de9410d9b14f3daa945ebe1cf634ffb114d7a
Bikash <info.bikash9@gmail.com>
2025-04-29 16:10:18 +0530

    Added test cases

--- src/test/java/com/automationteststore/test/HomePageTest.java
+++ src/test/java/com/automationteststore/test/HomePageTest.java
@@ -1,5 +1,21 @@
 package com.automationteststore.test;
 
-public class HomePageTest {
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import com.automationteststore.base.TestBase;
+import com.automationteststore.pages.HomePage;
+
+public class HomePageTest extends TestBase {
+	HomePage hpage;
+
+	@BeforeMethod
+	public void initialize() {
+		hpage = new HomePage(driver);
+	}
+
+	@Test
+	public void validateLoginPageUrlTest() {
+		hpage.clickOn_Login_RegisterBtn();
+	}
 }
