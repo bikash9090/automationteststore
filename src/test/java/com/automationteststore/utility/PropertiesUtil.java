090a698ac2fc3eddab0bcfc7186f766cba91ae55
Bikash <info.bikash9@gmail.com>
2025-05-09 11:31:31 +0530

    Added properties file reading methods.

--- src/test/java/com/automationteststore/utility/PropertiesUtil.java
+++ src/test/java/com/automationteststore/utility/PropertiesUtil.java
@@ -1,11 +1,32 @@
 package com.automationteststore.utility;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Properties;
+
+import com.automationteststore.base.Constants;
 
 public class PropertiesUtil {
 	Properties propObj;
 
 	public PropertiesUtil() {
-		
+		String path = Constants.PROPERTIES_FILE_PATH;
+		FileInputStream fis;
+		propObj = new Properties();
+
+		try {
+			fis = new FileInputStream(path);
+		} catch (FileNotFoundException e) {
+			System.out.println("Properties file not found!");
+			e.printStackTrace();
+		}
+	}
+
+	public String getURL() {
+		return propObj.getProperty("url");
+	}
+
+	public String getBrowser() {
+		return propObj.getProperty("browser");
 	}
 }
